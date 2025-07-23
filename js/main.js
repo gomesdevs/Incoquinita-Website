7e69bf3f69d4139a5b5b0f2ba7516ee82987d287
David <fenyxrainbow@protonmail.com>
2025-07-22 21:12:26 -0300

    feat(refazendo): literalmente voltando da escata zero para construimos esse site com uma nova stack

--- js/main.js
+++
@@ -1,6 +0,0 @@
-const btnDarkMode = document.querySelector('.dark-mode-btn');
-
-btnDarkMode.onclick = function () {
-    btnDarkMode.classList.toggle('dark-mode-btn__active');
-    document.body.classList.toggle('dark');
-};
